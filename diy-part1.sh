aae6c6cc2d0a475553e780e30cdb2268ffc271da
sidpixel <45841651+sidpixel@users.noreply.github.com>
2023-10-15 22:49:25 +0800

    add oaf

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 拉取oaf流控插件
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
