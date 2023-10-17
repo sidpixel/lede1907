0b0715a3849e35c0949d14073eed14751489ccf5
sidpixel <45841651+sidpixel@users.noreply.github.com>
2023-10-17 09:09:54 +0800

    Update diy-part1.sh
    add passwall source

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,7 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 # 拉取oaf流控插件
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
